231e149d801986295a65029085609c5f8df1434b
Diab Jerius <djerius@cfa.harvard.edu>
2016-11-22 14:15:17 -0500

    initial import

---
+++ cpanfile
@@ -0,0 +1,33 @@
+requires "Beam::Emitter" => "1.004";
+requires "Moo" => "0";
+requires "Safe::Isa" => "0";
+requires "Types::Standard" => "0";
+requires "strict" => "0";
+requires "warnings" => "0";
+
+on 'test' => sub {
+  requires "ExtUtils::MakeMaker" => "0";
+  requires "File::Spec" => "0";
+  requires "IO::Handle" => "0";
+  requires "IPC::Open3" => "0";
+  requires "Test::More" => "0";
+};
+
+on 'test' => sub {
+  recommends "CPAN::Meta" => "2.120900";
+};
+
+on 'configure' => sub {
+  requires "ExtUtils::MakeMaker" => "0";
+};
+
+on 'develop' => sub {
+  requires "Pod::Coverage::TrustPod" => "0";
+  requires "Test::CPAN::Changes" => "0";
+  requires "Test::CPAN::Meta" => "0";
+  requires "Test::Fixme" => "0";
+  requires "Test::NoBreakpoints" => "0";
+  requires "Test::Perl::Critic" => "0";
+  requires "Test::Pod" => "1.41";
+  requires "Test::Pod::Coverage" => "1.08";
+};
---
+++ lib/BeamX/Peer/Listener.pm
@@ -0,0 +1,50 @@
+package BeamX::Peer::Listener;
+
+use Types::Standard ':all';
+use Moo;
+
+our $VERSION = '0.001';
+
+extends 'Beam::Listener';
+
+=attr peer
+
+An optional reference to a peer object, allowing the emitter to
+send it an event directly using L<BeamX::Emitter::send>.  The 
+object must consume the B<BeamX::Emitter> role.
+
+=cut
+
+=method has_peer
+
+  $bool = $self->has_peer();
+
+This returns true if the object's peer attribute has been set
+
+=cut
+
+has peer => (
+    is       => 'ro',
+    weak_ref => 1,
+    isa      => ConsumerOf ['BeamX::Peer::Emitter'],
+    predicate => 1
+);
+
+1;
+
+# COPYRIGHT
+
+__END__
+
+=head1 SYNOPSIS
+
+  # optional peer
+  %args = ( peer => $peer );
+  $emitter->subscribe( $event_name, $subref, %args );
+
+
+=head1 DESCRIPTION
+
+This is the default Listener object created by the
+L<BeamX::Emitter::subscribe> method when a callback subscription is
+registered.  It sub-classes L<Beam::Listener>.
